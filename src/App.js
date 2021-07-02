33f5cb984db579046d877af9c046f0cf4dc2a25a
goldcvt <thegoldcat5@gmail.com>
2021-07-02 18:44:36 +0300

    edit components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Technologies />
     </div>
   );
 }
+
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          React
+        </li>
+        <li>
+          Redux
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <div>
+      <a href="#s">Home</a>
+      <a href="#s">Feed</a>
+      <a href="#s">Messages</a>
+    </div>
+  )
+}
+
+const ReactBoilerplate = () => (
+<header className="App-header">
+<img src={logo} className="App-logo" alt="logo" />
+<p>
+  <code>JSX for gangsta life!</code>
+</p>
+<p>
+  <a className="App-link" href="https://vk.com">Pull</a>
+   &nbsp;out and spin to reload!
+</p>
+</header>
+);
 
 export default App;
