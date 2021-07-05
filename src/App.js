559547dde3e104271977f888b2595b297fd58a5e
goldcvt <thegoldcat5@gmail.com>
2021-07-06 02:26:21 +0300

    Add router for profile and dialogs, reworked content part of page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
+import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-const App = () => {
+const App = (props) => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar/>
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar/>
+        <div className="app-wrapper-content">
+          <Route component={Profile} path="/profile" />
+          <Route component={Dialogs} path="/dialogs" />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
