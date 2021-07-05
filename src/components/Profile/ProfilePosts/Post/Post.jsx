d64fa64e6fbbbf60411eaf8d000dace80b70ef62
goldcvt <thegoldcat5@gmail.com>
2021-07-05 16:31:18 +0300

    Add first props

--- src/components/Profile/ProfilePosts/Post/Post.jsx
+++ src/components/Profile/ProfilePosts/Post/Post.jsx
@@ -1,15 +1,22 @@
 import stylesheet from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={stylesheet.post}>
             <div className={stylesheet.post_content}>
-                <img src='https://i.ytimg.com/vi/BHPPLdWeGiw/maxresdefault.jpg' alt="Author's Avatar" className={stylesheet.avatar} />
-                <p className={stylesheet.post_text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam obcaecati illum molestias quidem quis non nesciunt soluta! Eligendi, nulla illum. Facilis aliquam ducimus voluptatum aspernatur dolores ullam sequi id aut!</p>
+                <div className={stylesheet.author}>
+                    <img src={props.author_avatar} alt="Author's Avatar" className={stylesheet.avatar} />
+                    <div className={stylesheet.name}>
+                        <p>
+                            {props.author}
+                        </p>
+                    </div>
+                </div>
+                <p className={stylesheet.post_text}>{props.text}</p>
             </div>
             <div className={stylesheet.nav}>
-                <span>Like</span>
-                <span>Share</span>
+                <span>Like {props.likes}</span>
+                <span>Share {props.shares}</span>
             </div>
         </div>
     );
--- src/components/Profile/ProfilePosts/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts/ProfilePosts.jsx
@@ -7,7 +7,8 @@ const ProfilePosts = () => {
         <div className = {stylesheet.prof_posts}>
             My posts
             <CreatePost />
-            <Post />
+            <Post text="Howdy, partner" author="Ragnar Lodbrok" author_avatar='https://i.ytimg.com/vi/BHPPLdWeGiw/maxresdefault.jpg' likes={312} shares={12} />
+            <Post text="Lorem ipsemum... You know the drill" author="Ragnar Lodbrok" author_avatar='https://i.ytimg.com/vi/BHPPLdWeGiw/maxresdefault.jpg' likes={228} shares={124}/>
         </div>
     )
 };
