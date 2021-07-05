23675e4577d749e3335bb159cc0fc687fc2e8148
goldcvt <thegoldcat5@gmail.com>
2021-07-06 02:29:01 +0300

    Add socail media sections like music feed etc

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Music from './components/Music/Music';
+import Feed from './components/Feed/Feed';
+import Settings from './components/Settings/Settings';
 
 const App = (props) => {
   return (
@@ -14,6 +17,9 @@ const App = (props) => {
         <div className="app-wrapper-content">
           <Route component={Profile} path="/profile" />
           <Route component={Dialogs} path="/dialogs" />
+          <Route component={Music} path="/music" />
+          <Route component={Feed} path="/feed" />
+          <Route component={Settings} path="/settings" />
         </div>
       </div>
     </BrowserRouter>
