53e9303f31809d37f22b63d28bc32093c8f120d6
goldcvt <thegoldcat5@gmail.com>
2021-07-09 00:43:27 +0300

    Made some dirty css dummy of avatar + description section

--- src/components/Profile/About/About.jsx
+++ src/components/Profile/About/About.jsx
@@ -2,11 +2,23 @@ import stylesheet from './About.module.css';
 
 const About = (props) => {
     return (
-        <div className = {stylesheet.about}>
-            <div>
+        <div className={stylesheet.about}>
+            <div className={stylesheet.wallpaper}>
                 <img src="https://wallpaperaccess.com/full/1760835.jpg" alt="Mountain View, not California" className={stylesheet.wallpaper} />
             </div>
-            <div>Ava + desc</div>
+            <div className={stylesheet.name}>
+                <h1>Ragnar Lodbrok</h1>
+            </div>
+
+            <div className={stylesheet.avatar_container}>
+                <img src="https://i.ytimg.com/vi/BHPPLdWeGiw/maxresdefault.jpg" alt="Beautiful Person" />
+            </div>
+            <div className={stylesheet.user_info_text}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Nihil ad adipisci illo fugit asperiores officia iste itaque, praesentium accusantium
+                laboriosam saepe tempora temporibus. Et incidunt architecto quaerat? Dolore, maxime itaque!
+            </div>
+
         </div>
     );
 }
