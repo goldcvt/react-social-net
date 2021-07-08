ebddd32f88f2e914f74f23ea1a3130fa44a3dfd0
goldcvt <thegoldcat5@gmail.com>
2021-07-09 02:02:00 +0300

    Refactor: splitted BLL from UI a bit

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,20 +3,35 @@ import Message from './Message/Message';
 import DialogItem from './DialogItem/DialogItem';
 
 const Dialogs = (props) => {
+
+    // let's imagine that this thing came into our props
+    const dialogItemsData = [
+        { userId: 1, name: "Brigitte Valentine" },
+        { userId: 2, name: "Actual Name" },
+        { userId: 3, name: "Not an Actual Name" },
+    ];
+
+    const messageData = [
+        { msgId: 0, text: "Fuck", toMe: true },
+        { msgId: 1, text: "I miss you!", toMe: true },
+        { msgId: 2, text: "Too bad", toMe: false }
+    ]
+
+    let messages = messageData.map(message => <Message message={message.text} toMe={message.toMe} />);
+
+    let dialogs = dialogItemsData.map(user => <DialogItem user={user} />);
+
     return (
         <div className={stylesheet.dialogs_page}>
             <div className={stylesheet.dialogs_selector}>
 
-                <DialogItem user={{ userId: 1, name: "Brigitte Valentine" }} />
-                <DialogItem user={{ userId: 2, name: "Actual Name" }} />
-                <DialogItem user={{ userId: 3, name: "Not an Actual Name" }} />
+                {dialogs}
 
             </div>
             <div className={stylesheet.dialog_window}>
-                {/* Need some sort of a loader here or something */}
-                <Message message="Fuck" toMe={true} />
-                <Message message="I miss you!" toMe={true} />
-                <Message message="Too bad." toMe={false} />
+                {/* Need some loader here or something */}
+                {messages}
+
             </div>
         </div>
     );
--- src/components/Profile/ProfilePosts/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts/ProfilePosts.jsx
@@ -3,12 +3,33 @@ import CreatePost from "./CreatePost/CreatePost";
 import Post from "./Post/Post"
 
 const ProfilePosts = (props) => {
+
+
+    // let's imagine posts came from props. And oh! author should be replaced with authorId
+    // And! Instead of getting all the posts as an array we could do something like generator expr to fetch them from server
+    let postsData = [
+        {
+            id: 0, text: "Howdy, partner", author: "Ragnar Lodbrok", avatarUrl: 'https://i.ytimg.com/vi/BHPPLdWeGiw/maxresdefault.jpg',
+            likes: 312, shares: 12
+        },
+        {
+            id: 1, text: "Lorem ipsemum... You know the drill", author: "Ragnar Lodbrok", avatarUrl: 'https://i.ytimg.com/vi/BHPPLdWeGiw/maxresdefault.jpg',
+            likes: 228, shares: 124
+        }
+    ];
+
+    let posts = postsData.map((post) => 
+        (<Post text={post.text} author={post.author} author_avatar={post.avatarUrl} likes={post.likes} shares={post.shares} />)
+    );
+
     return (
         <div className = {stylesheet.prof_posts}>
-            My posts
+            <div className={stylesheet.header_container}>
+                <h1>My posts</h1>
+            </div>
             <CreatePost />
-            <Post text="Howdy, partner" author="Ragnar Lodbrok" author_avatar='https://i.ytimg.com/vi/BHPPLdWeGiw/maxresdefault.jpg' likes={312} shares={12} />
-            <Post text="Lorem ipsemum... You know the drill" author="Ragnar Lodbrok" author_avatar='https://i.ytimg.com/vi/BHPPLdWeGiw/maxresdefault.jpg' likes={228} shares={124}/>
+            
+            { posts }
         </div>
     )
 };
