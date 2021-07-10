50545751df06a1970778878dda44477d855e8836
goldcvt <thegoldcat5@gmail.com>
2021-07-11 00:54:05 +0300

    Add box to send messages

---
+++ src/components/Dialogs/DialogItem/SendMessage/SendMessage.jsx
@@ -0,0 +1,14 @@
+import stylesheet from './SendMessage.module.css';
+
+const SendMessage = (props) => {
+    return (
+        <div className={stylesheet.container}>
+            <div className={stylesheet.field}>
+                <input type="text" />
+            </div>
+            <div className={stylesheet.button_wrapper}><button className={stylesheet.button}>></button></div>
+        </div>
+    )
+}
+
+export default SendMessage;
