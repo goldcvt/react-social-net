17a42462b3c353daf332659a33f64b1483d25324
goldcvt <thegoldcat5@gmail.com>
2021-07-18 02:57:22 +0300

    Make action types constants, make action creators

---
+++ src/redux/actionCreators.js
@@ -0,0 +1,11 @@
+import { DIALOGS_CREATE_MESSAGE, DIALOGS_UPDATE_CURRENT_MESSAGE, 
+    PROFILE_CREATE_POST, PROFILE_UPDATE_CURRENT_POST } from "./actionTypes"
+
+
+export const createMessage = (payload) => ({type: DIALOGS_CREATE_MESSAGE, payload: payload})
+
+export const updateCurrentMessage = (payload) => ({type: DIALOGS_UPDATE_CURRENT_MESSAGE, payload: payload})
+
+export const createPost = (payload) => ({type: PROFILE_CREATE_POST, payload: payload})
+
+export const updateCurrentPost = (payload) => ({type: PROFILE_UPDATE_CURRENT_POST, payload: payload})
