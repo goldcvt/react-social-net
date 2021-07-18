0425485e71b6605e2695ea64030a2f6cae36088c
goldcvt <thegoldcat5@gmail.com>
2021-07-19 02:17:24 +0300

    make profileReducer

--- src/redux/reducers/profileReducer.js
+++ src/redux/reducers/profileReducer.js
@@ -1,6 +1,24 @@
-const profileReducer = (state, action) => {
+import { PROFILE_CREATE_POST, PROFILE_LIKE_POST, PROFILE_UPDATE_CURRENT_POST } from "../actionTypes";
+import { addPost, like, typingPost } from "../profileFunctions";
 
-    return state;
+const profileReducer = (state, action) => {
+    switch (action.type) {
+        case PROFILE_CREATE_POST:
+            addPost(state.postsData, state.currentPost.text, ...action.payload);
+            typingPost(state.currentPost, "");
+            return state
+
+        case PROFILE_UPDATE_CURRENT_POST:
+            typingPost(state.currentPost, ...action.payload);
+            return state
+
+        case PROFILE_LIKE_POST:
+            like(...action.payload)
+            return state
+
+        default:
+            return state
+    }
 }
 
 export default profileReducer;
