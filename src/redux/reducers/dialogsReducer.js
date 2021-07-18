99b08a0d89aa49e3ec5aecd28c77665a8cf013bc
goldcvt <thegoldcat5@gmail.com>
2021-07-19 02:16:51 +0300

    make dialogsReducer

--- src/redux/reducers/dialogsReducer.js
+++ src/redux/reducers/dialogsReducer.js
@@ -1,6 +1,21 @@
-const dialogsReducer = (state, action) => {
+import { DIALOGS_CREATE_MESSAGE, DIALOGS_UPDATE_CURRENT_MESSAGE } from "../actionTypes"
+import { addMessage, typingMessage } from "../dialogFunctions"
 
-    return state;
+
+const dialogsReducer = (state, action) => {
+    switch (action.type) {
+        case DIALOGS_CREATE_MESSAGE:
+            addMessage(state.messageData, state.currentMessage.text)
+            typingMessage(state.currentMessage, "")
+            return state
+
+        case DIALOGS_UPDATE_CURRENT_MESSAGE:
+            typingMessage(state.currentMessage, ...action.payload)
+            return state
+
+        default:
+            return state
+    }
 }
 
-export default dialogsReducer;
+export default dialogsReducer
