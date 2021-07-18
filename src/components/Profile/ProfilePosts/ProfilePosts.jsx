366e397220aeedd81f0dbcc763a8f3c9434fa5ca
goldcvt <thegoldcat5@gmail.com>
2021-07-18 03:03:33 +0300

    Fix posts display: earlier posts come first

--- src/components/Profile/ProfilePosts/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts/ProfilePosts.jsx
@@ -6,19 +6,21 @@ const ProfilePosts = (props) => {
     // let's imagine posts came from props. And oh! author should be replaced with authorId
     // And! Instead of getting all the posts as an array we could do something like generator expr to fetch them from server
 
-    let posts = props.state.postsData.map((post) => 
+    let posts = props.state.postsData.map((post) =>
         (<Post text={post.text} author={post.author} author_avatar={post.avatarUrl} likes={post.likes} shares={post.shares} />)
     );
 
     return (
-        <div className = {stylesheet.prof_posts}>
+        <div className={stylesheet.prof_posts}>
             <div className={stylesheet.header_container}>
                 <h1>My posts</h1>
             </div>
-            <CreatePost dispatch={props.dispatch} currentPost={props.state.currentPost} 
-            posts={props.state.postsData} userData ={props.state.userData} />
-            
-            { posts }
+            <CreatePost dispatch={props.dispatch} currentPost={props.state.currentPost}
+                posts={props.state.postsData} userData={props.state.userData} />
+
+            <div className={stylesheet.posts}>
+                {posts}
+            </div>
         </div>
     )
 };
