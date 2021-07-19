f205f2b10eeb3cfe2b3d673a691fc0b8f352bcdd
goldcvt <thegoldcat5@gmail.com>
2021-07-19 20:45:32 +0300

    Add send message container componenet

---
+++ src/components/Dialogs/SendMessage/SendMessageContainer.jsx
@@ -0,0 +1,20 @@
+import { createMessage, updateCurrentMessage } from '../../../redux/actionCreators';
+import SendMessage from './SendMessage'
+
+const SendMessageContainer = (props) => {
+    let sendMessage = () => {
+        let action = createMessage([""])
+        props.dispatch(action);
+        // props.functions.changeCurrent(props.state.currentMessage, "");
+    }
+
+    let typeChars = (text) => {
+        let payload = [text]
+        let action = updateCurrentMessage(payload)
+        props.dispatch(action);
+    }
+
+    return <SendMessage typeMessage={typeChars} currentMessage={props.state.currentMessage} sendMessage={sendMessage} />
+}
+
+export default SendMessageContainer;
