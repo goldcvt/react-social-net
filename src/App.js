cfe39f161515ddf9f1da444f1ae7ffb851b1f31b
goldcvt <thegoldcat5@gmail.com>
2021-07-20 02:56:12 +0300

    Add Provider to app

--- src/App.js
+++ src/App.js
@@ -7,25 +7,28 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Music from './components/Music/Music';
 import Feed from './components/Feed/Feed';
 import Settings from './components/Settings/Settings';
+import { Provider } from 'react-redux';
+import store from './redux/reduxStore';
 
 const App = (props) => {
 
   return (
     <BrowserRouter>
-      <div className='app-wrapper'>
-        <Header />
-        <Navbar state={props.store.getState().friends} />
-        <div className="app-wrapper-content">
-          <Route render={() => <Profile state={props.store.getState().profile}
-            dispatch={props.store.dispatch.bind(props.store)} />} path="/profile" />
-
-          <Route render={() => <Dialogs state={props.store.getState().dialogs}
-            dispatch={props.store.dispatch.bind(props.store)} />} path="/dialogs" />
-          <Route component={Music} path="/music" />
-          <Route component={Feed} path="/feed" />
-          <Route component={Settings} path="/settings" />
+      <Provider store={store}>
+        <div className='app-wrapper'>
+          <Header />
+          <Navbar state={props.store.getState().friends} />
+          <div className="app-wrapper-content">
+            <Route render={() => <Profile state={props.store.getState().profile}
+              dispatch={props.store.dispatch.bind(props.store)} />} path="/profile" />
+            <Route render={() => <Dialogs state={props.store.getState().dialogs}
+              dispatch={props.store.dispatch.bind(props.store)} />} path="/dialogs" />
+            <Route component={Music} path="/music" />
+            <Route component={Feed} path="/feed" />
+            <Route component={Settings} path="/settings" />
+          </div>
         </div>
-      </div>
+      </Provider>
     </BrowserRouter>
   );
 }
