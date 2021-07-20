ae924dbd3d5b6ca4574bcd3a2b3148175fe29908
goldcvt <thegoldcat5@gmail.com>
2021-07-20 21:58:44 +0300

    fix deepCopy function + export

--- src/utils/deepCopy.js
+++ src/utils/deepCopy.js
@@ -1,6 +1,14 @@
 const deepCopy = (plainObj) => {
     let keys = Object.keys(plainObj)
-    let objCopy = {...plainObj}
+    // if array - make array)
+    let objCopy = null;
+    if (Array.isArray(plainObj)) {
+        objCopy = [...plainObj]
+    } else {
+        objCopy = {
+            ...plainObj
+        }
+    }
     for (let key of keys) {
         if (plainObj[key] != null) {
             if (typeof plainObj[key] === "object") {
@@ -11,3 +19,5 @@ const deepCopy = (plainObj) => {
 
     return objCopy
 }
+
+export default deepCopy;
