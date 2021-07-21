5fb37af77012c353232c8f76127253768686871a
goldcvt <thegoldcat5@gmail.com>
2021-07-21 21:23:51 +0300

    Made UserItem component's base layout + styles

--- src/components/Users/UserItem/UserItem.jsx
+++ src/components/Users/UserItem/UserItem.jsx
@@ -1,6 +1,17 @@
+import { info_wrapper, name_wrapper, friend, avatar_wrapper } from './UserItem.module.scss'
+
 const UserItem = (props) => {
     return (
-        <div className={""}>{props.user.name}</div>
+        <div className={friend}>
+            <div className={avatar_wrapper}>
+                <img src={props.user.avatarUrl} alt={`${props.user.name}'s Avatar`}  />
+            </div>
+            <div className={info_wrapper}>
+                <div className={name_wrapper}>
+                    <p>{props.user.name}</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
