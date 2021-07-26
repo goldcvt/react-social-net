5a19241369820ef74ac16e5e67426838829b7001
goldcvt <thegoldcat5@gmail.com>
2021-07-26 19:36:40 +0300

    add parameters handling in loadUsers(), wrap page in a pagination div

--- src/components/Users/UsersClass.jsx
+++ src/components/Users/UsersClass.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Search from '../Utils/Search/Search'
 import UserItem from './UserItem/UserItem'
-import { allUsers_title, userscontainer, userlist, searchbar, filters, friends_title } from './Users.module.scss'
+import { page_container, allUsers_title, userscontainer, userlist, searchbar, filters, friends_title } from './Users.module.scss'
 import * as axios from 'axios'
 
 class Users extends React.Component {
-    loadUsers = () => {
+    loadUsers = (params = null) => {
+        let getQuery = params ? Object.entries(params).reduce((accum, cur) => {
+            return accum + `${cur[0]}=${cur[1]}`
+        }, `?`) : ""
         let props = this.props
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(function(response) {
+        console.log(getQuery)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users${getQuery}`).then(function(response) {
             props.fetchUsers(response.data.items)
         })
     }
@@ -15,28 +19,30 @@ class Users extends React.Component {
         let friends = this.props.users.filter(user => user.followed === true).map(user => <UserItem user={user} toggleFriendship={this.props.toggleFriendship} />)
         let notYetFriends = this.props.users.filter(user => user.followed !== true).map(user => <UserItem user={user} toggleFriendship={this.props.toggleFriendship} />)
         return (
-            <div className={userscontainer}>
-                <div className={userlist}>
-                    <div className={searchbar}>
-                        <Search placeholder="Looking for someone special?" />
+            <div className={page_container}>
+                <div className={userscontainer}>
+                    <div className={userlist}>
+                        <div className={searchbar}>
+                            <Search placeholder="Looking for someone special?" />
+                        </div>
+                        <div className={friends_title}>
+                            <h2>Friends</h2>
+                        </div>
+                        {friends}
+                        <div className={allUsers_title}>
+                            <h2>
+                                Not Friends Yet
+                            </h2>
+                        </div>
+                        {notYetFriends}
+                        <button onClick={this.loadUsers}>Show More</button>
                     </div>
-                    <div className={friends_title}>
-                        <h2>Friends</h2>
+                    <div className={filters}>
+                        <h1>Filters</h1>
+                        <p>City</p>
+                        <p>State</p>
+                        <p>Whatever</p>
                     </div>
-                    {friends}
-                    <div className={allUsers_title}>
-                        <h2>
-                            Not Friends Yet
-                        </h2>
-                    </div>
-                    {notYetFriends}
-                    <button onClick={this.loadUsers}>Show More</button>
-                </div>
-                <div className={filters}>
-                    <h1>Filters</h1>
-                    <p>City</p>
-                    <p>State</p>
-                    <p>Whatever</p>
                 </div>
             </div>
         )
