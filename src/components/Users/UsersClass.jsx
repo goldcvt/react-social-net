7ffba1d01f05ec9efd917a5ae1fb0e018876b78b
goldcvt <thegoldcat5@gmail.com>
2021-07-27 17:25:46 +0300

    add pagination, dispatch fetchUsers, remove reduceUsers dispatching from component

--- src/components/Users/UsersClass.jsx
+++ src/components/Users/UsersClass.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Search from '../Utils/Search/Search'
 import UserItem from './UserItem/UserItem'
-import { page_container, allUsers_title, userscontainer, userlist, searchbar, filters, friends_title } from './Users.module.scss'
+import { paginator, current_page, page_number, page_container, allUsers_title, userscontainer, userlist, searchbar, filters, friends_title } from './Users.module.scss'
 import * as axios from 'axios'
+import { NavLink } from "react-router-dom";
 
 class Users extends React.Component {
     loadUsers = (params = null) => {
@@ -11,11 +12,73 @@ class Users extends React.Component {
         }, `?`) : ""
         let props = this.props
         console.log(getQuery)
+        let pageNumber = params ? params.page : props.currentPage
         axios.get(`https://social-network.samuraijs.com/api/1.0/users${getQuery}`).then(function(response) {
-            props.fetchUsers(response.data.items)
+            let payload = {...response.data, pageNumber}
+            props.fetchUsers(payload)
         })
     }
     render() {
+        const createPaginator = () => {
+            let pagebar = []
+            
+            for (let index = 1; index <= this.props.totalPages; index++) {
+                
+                pagebar.push(
+                    <div>
+                        <NavLink to={index === 1 ? `/users` : `/users?page=${index}`} activeClassName={current_page} >
+                            <div className={page_number} onClick={
+                                () => {
+                                    this.loadUsers({ page: index })
+                                }
+                            }
+                            >{index}</div>
+                        </NavLink>
+                    </div>
+                )
+            }
+            let currentPage = this.props.currentPage
+            let totalPages = this.props.totalPages
+            if ((currentPage >= 5) && (currentPage <= totalPages - 4)) {
+                let rightEnd = currentPage + 2
+                let leftEnd = currentPage - 2
+                pagebar = [pagebar[0]].concat(
+                    <div>
+                        <div className={page_number}>...</div>
+                    </div>
+                ).concat(
+                    pagebar.slice(leftEnd - 1, rightEnd)
+                ).concat(
+                    <div>
+                        <div className={page_number}>...</div>
+                    </div>
+                ).concat(
+                    [pagebar[pagebar.length - 1]]
+                )
+            } else if (currentPage < 5) {
+                let rightEnd = currentPage + 2
+                pagebar = pagebar.slice(0, rightEnd).concat(
+                    <div>
+                        <div className={page_number}>...</div>
+                    </div>
+                ).concat(
+                    [pagebar[pagebar.length - 1]]
+                )
+            } else if (currentPage > totalPages - 4) {
+                let leftEnd = currentPage - 2
+                pagebar = [pagebar[0]].concat(
+                    <div>
+                        <div className={page_number}>...</div>
+                    </div>
+                ).concat(
+                    pagebar.slice(leftEnd - 1, totalPages)
+                )
+            }
+            return pagebar
+        }
+
+        let pagebar = createPaginator()
+
         let friends = this.props.users.filter(user => user.followed === true).map(user => <UserItem user={user} toggleFriendship={this.props.toggleFriendship} />)
         let notYetFriends = this.props.users.filter(user => user.followed !== true).map(user => <UserItem user={user} toggleFriendship={this.props.toggleFriendship} />)
         return (
@@ -44,12 +107,9 @@ class Users extends React.Component {
                         <p>Whatever</p>
                     </div>
                 </div>
+                <div className={paginator}>{pagebar}</div>
             </div>
         )
-    }
-    componentWillUnmount() {
-        // don't clutter state with all other users, will remove with pagination
-        this.props.reduceUsers([])
     }
     componentDidMount() {
         this.loadUsers()
